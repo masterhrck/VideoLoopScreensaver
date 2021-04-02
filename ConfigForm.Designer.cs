78f0b8ab6418061d40bb369ee767d3f35abfcee4
Hrvoje Lucianović <masterhrck@gmail.com>
2021-04-02 16:53:31 +0200

    Enlarged buttons

--- ConfigForm.Designer.cs
+++ ConfigForm.Designer.cs
@@ -46,16 +46,18 @@ namespace VideoLoopScreensaver
 			// 
 			// textBoxVideoPath
 			// 
-			this.textBoxVideoPath.Location = new System.Drawing.Point(6, 26);
+			this.textBoxVideoPath.Location = new System.Drawing.Point(6, 21);
+			this.textBoxVideoPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.textBoxVideoPath.Name = "textBoxVideoPath";
-			this.textBoxVideoPath.Size = new System.Drawing.Size(490, 27);
+			this.textBoxVideoPath.Size = new System.Drawing.Size(490, 22);
 			this.textBoxVideoPath.TabIndex = 0;
 			// 
 			// buttonBrowse
 			// 
-			this.buttonBrowse.Location = new System.Drawing.Point(502, 26);
+			this.buttonBrowse.Location = new System.Drawing.Point(502, 17);
+			this.buttonBrowse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.buttonBrowse.Name = "buttonBrowse";
-			this.buttonBrowse.Size = new System.Drawing.Size(94, 29);
+			this.buttonBrowse.Size = new System.Drawing.Size(94, 30);
 			this.buttonBrowse.TabIndex = 1;
 			this.buttonBrowse.Text = "Browse";
 			this.buttonBrowse.UseVisualStyleBackColor = true;
@@ -63,7 +65,8 @@ namespace VideoLoopScreensaver
 			// 
 			// trackBarVolume
 			// 
-			this.trackBarVolume.Location = new System.Drawing.Point(6, 26);
+			this.trackBarVolume.Location = new System.Drawing.Point(6, 21);
+			this.trackBarVolume.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.trackBarVolume.Maximum = 100;
 			this.trackBarVolume.Name = "trackBarVolume";
 			this.trackBarVolume.Size = new System.Drawing.Size(484, 56);
@@ -74,9 +77,11 @@ namespace VideoLoopScreensaver
 			// 
 			this.groupBoxVideoPath.Controls.Add(this.textBoxVideoPath);
 			this.groupBoxVideoPath.Controls.Add(this.buttonBrowse);
-			this.groupBoxVideoPath.Location = new System.Drawing.Point(12, 12);
+			this.groupBoxVideoPath.Location = new System.Drawing.Point(12, 10);
+			this.groupBoxVideoPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.groupBoxVideoPath.Name = "groupBoxVideoPath";
-			this.groupBoxVideoPath.Size = new System.Drawing.Size(611, 69);
+			this.groupBoxVideoPath.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.groupBoxVideoPath.Size = new System.Drawing.Size(611, 58);
 			this.groupBoxVideoPath.TabIndex = 3;
 			this.groupBoxVideoPath.TabStop = false;
 			this.groupBoxVideoPath.Text = "Video file path";
@@ -85,9 +90,11 @@ namespace VideoLoopScreensaver
 			// 
 			this.groupBoxVolume.Controls.Add(this.checkBoxVolumeMute);
 			this.groupBoxVolume.Controls.Add(this.trackBarVolume);
-			this.groupBoxVolume.Location = new System.Drawing.Point(12, 87);
+			this.groupBoxVolume.Location = new System.Drawing.Point(12, 72);
+			this.groupBoxVolume.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.groupBoxVolume.Name = "groupBoxVolume";
-			this.groupBoxVolume.Size = new System.Drawing.Size(611, 90);
+			this.groupBoxVolume.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.groupBoxVolume.Size = new System.Drawing.Size(611, 79);
 			this.groupBoxVolume.TabIndex = 4;
 			this.groupBoxVolume.TabStop = false;
 			this.groupBoxVolume.Text = "Volume";
@@ -95,9 +102,10 @@ namespace VideoLoopScreensaver
 			// checkBoxVolumeMute
 			// 
 			this.checkBoxVolumeMute.AutoSize = true;
-			this.checkBoxVolumeMute.Location = new System.Drawing.Point(497, 27);
+			this.checkBoxVolumeMute.Location = new System.Drawing.Point(497, 22);
+			this.checkBoxVolumeMute.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.checkBoxVolumeMute.Name = "checkBoxVolumeMute";
-			this.checkBoxVolumeMute.Size = new System.Drawing.Size(65, 24);
+			this.checkBoxVolumeMute.Size = new System.Drawing.Size(61, 21);
 			this.checkBoxVolumeMute.TabIndex = 3;
 			this.checkBoxVolumeMute.Text = "Mute";
 			this.checkBoxVolumeMute.UseVisualStyleBackColor = true;
@@ -108,18 +116,20 @@ namespace VideoLoopScreensaver
 			this.checkBoxMouseExit.AutoSize = true;
 			this.checkBoxMouseExit.Checked = true;
 			this.checkBoxMouseExit.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.checkBoxMouseExit.Location = new System.Drawing.Point(12, 183);
+			this.checkBoxMouseExit.Location = new System.Drawing.Point(12, 168);
+			this.checkBoxMouseExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.checkBoxMouseExit.Name = "checkBoxMouseExit";
-			this.checkBoxMouseExit.Size = new System.Drawing.Size(280, 24);
+			this.checkBoxMouseExit.Size = new System.Drawing.Size(272, 21);
 			this.checkBoxMouseExit.TabIndex = 5;
 			this.checkBoxMouseExit.Text = "End screensaver on mouse movement";
 			this.checkBoxMouseExit.UseVisualStyleBackColor = true;
 			// 
 			// buttonSave
 			// 
-			this.buttonSave.Location = new System.Drawing.Point(429, 246);
+			this.buttonSave.Location = new System.Drawing.Point(429, 197);
+			this.buttonSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.buttonSave.Name = "buttonSave";
-			this.buttonSave.Size = new System.Drawing.Size(94, 29);
+			this.buttonSave.Size = new System.Drawing.Size(94, 30);
 			this.buttonSave.TabIndex = 6;
 			this.buttonSave.Text = "Save";
 			this.buttonSave.UseVisualStyleBackColor = true;
@@ -127,9 +137,11 @@ namespace VideoLoopScreensaver
 			// 
 			// buttonCancel
 			// 
-			this.buttonCancel.Location = new System.Drawing.Point(529, 246);
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(529, 197);
+			this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(94, 29);
+			this.buttonCancel.Size = new System.Drawing.Size(94, 30);
 			this.buttonCancel.TabIndex = 7;
 			this.buttonCancel.Text = "Cancel";
 			this.buttonCancel.UseVisualStyleBackColor = true;
@@ -142,16 +154,17 @@ namespace VideoLoopScreensaver
 			// ConfigForm
 			// 
 			this.AcceptButton = this.buttonSave;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(644, 289);
+			this.ClientSize = new System.Drawing.Size(644, 248);
 			this.Controls.Add(this.buttonCancel);
 			this.Controls.Add(this.buttonSave);
 			this.Controls.Add(this.checkBoxMouseExit);
 			this.Controls.Add(this.groupBoxVolume);
 			this.Controls.Add(this.groupBoxVideoPath);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.MaximizeBox = false;
 			this.Name = "ConfigForm";
 			this.Text = "Configuration";
